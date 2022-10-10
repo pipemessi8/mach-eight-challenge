1030f18b017cfed3e716a8559ccef780db64d4dd
Felipe Mesa <pipemessi8@gmail.com>
2022-10-09 21:29:07 -0500

    feat: findPairs

---
+++ README.md
@@ -0,0 +1,24 @@
+## Dependecies
+
+* Any node version.
+
+## How to Run
+
+Type in your favorite terminal the next code adding your variable values
+```
+node app {array} {sum}
+```
+## Example:
+
+```
+node app 1,9,5,0,20,-4,12,16,7 12
+```
+​
+This will be the expected result.
+```
++ -4,16
++ 0,12
++ 5,7
+​
+```
+​
---
+++ app.js
@@ -0,0 +1,38 @@
+const findPairs = (elements, sum) => {
+  let size = elements.length;
+  elements = elements.sort((a, b) => a - b);
+  let countOut = 0;
+  for (let x = 0; x < size; x++) {
+    let currentSum = 0;
+    let numA = elements[x];
+    let numB = 0;
+    for (let y = x + 1; y < size; y++) {
+      numB = elements[y];
+      currentSum = numA + numB;
+      if (currentSum === sum) {
+        console.log(`+ ${numA},${numB}`);
+      } else if (currentSum > sum) {
+        countOut++;
+        break;
+      } else {
+        countOut = 0;
+      }
+    }
+    if (countOut === 2) {
+      break;
+    }
+  }
+};
+
+const main = () => {
+  try {
+    const elements = process.argv[2].split(",").map((num) => parseInt(num));
+    const sum = parseInt(process.argv[3]);
+    const test = parseInt(process.argv[3]);
+    findPairs(elements, sum);
+  } catch (error) {
+    console.log(`Some parameters could't be processed`);
+  }
+};
+
+main();
